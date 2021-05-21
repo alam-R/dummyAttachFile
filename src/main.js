08cacef4e0374a586b55c277848ca4af7852e9a5
tasos <alam@intracom-telecom.com>
2021-05-21 15:48:58 +0300

    disrespectful arguments

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 export default function dummyAttachFile(subject, sizeBytes = 1024, fileName = 'whatever.txt', fileType = 'text/plain') {
 
+    // if arguments do not respect their type
+    // convert them to defaults
+    if (typeof sizeBytes !== 'number') {
+        sizeBytes = 1024
+    }
+    if (typeof fileName !== 'string') {
+        fileName = 'whatever.txt'
+    }
+    if (typeof fileType !== 'string') {
+        fileType = 'text/plain'
+    }
+
     const arrBuffer = new ArrayBuffer(sizeBytes)
     const el = subject[0]
     const testFile = new File([arrBuffer], fileName, {type: fileType})
